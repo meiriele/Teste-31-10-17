9b68b13bb7916d4926cd08a9cda36069b325bfd5
TI <meiriele_dias@yahoo.com.br>
2017-10-31 16:12:52 -0200

    Teste busca arquivos

---
+++ src/ufjf/br/Busca.java
@@ -0,0 +1,20 @@
+
+package ufjf.br;
+
+import javax.swing.JFrame;
+
+
+public class Busca {
+    
+
+    
+    public static void main(String[] args) {
+        NewJFrame janela = new NewJFrame();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(400, 300);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+
+    }
+    
+}
